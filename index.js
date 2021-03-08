adabc7f500acd7bdaa0f68adb1e079c8b112a726
MiguelED <migueled2695@gmail.com>
2021-03-08 11:06:46 -0600

    suma de todos los elementos

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict";
+var suma_de_elementos = function (datos) {
+    return datos.reduce(function (acumulador, elemento) { return acumulador + elemento; }, 0);
+};
+var datos = suma_de_elementos([3, 6, 1, 4, 5]);
+console.log("la suma de todos los elementos es " + datos);
