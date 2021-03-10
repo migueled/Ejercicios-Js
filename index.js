201d8864b94c9d994c585c1bf4a963a2f1b86f9c
MiguelED <migueled2695@gmail.com>
2021-03-09 19:46:40 -0600

    minimo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 "use strict";
 var arr = [1, 2, 3, 9, 12, 3, 27];
 var max = Math.max.apply(Math, arr);
-console.log(max);
+var min = Math.min.apply(Math, arr);
+console.log("El valor max es " + max + " el valor minimo es " + min);
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 const arr = [1, 2, 3, 9 ,12 , 3 ,27];
 const max = Math.max(...arr);
-console.log(max);
+const min = Math.min(...arr)
+console.log(`El valor max es ${max} el valor minimo es ${min}`);
