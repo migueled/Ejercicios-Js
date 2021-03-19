1dc5a862b6e24fdfe526fc043afd14a4838917a8
MiguelED <migueled2695@gmail.com>
2021-03-18 18:57:22 -0600

    Filtros con *

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 "use strict";
-var altura = 1.75;
-var peso = 56;
-/*peso/kilogramoos^2*/
-var IMC = function (peso, altura) {
-    return "El IMC es " + peso / (Math.pow(altura, 2));
-};
-var resultado = IMC(65, 1.60);
-console.log(resultado);
+var myArr = ['a', '*', 'l', '*', 'o', 'H'];
+var Filter = myArr.filter(function (elemento) { return elemento != '*'; }).reverse().join('');
+console.log(Filter);
